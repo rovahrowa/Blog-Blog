c08749b8d2c9510feeae7916d2e8c85a261918d7
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-05-16 10:22:10 +0300

    Adds the default api route for ther endpoints

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,13 +1,22 @@
 /**
  * Created by danstan on 5/13/17.
  */
+
 let apiRoutes = (app) =>{
-    app.get('/api',function (rq,rs) {
-        rs.json({
-            saying:"I love js"
+    app.get('/api', function (req, res) {
+        res.send({
+
+            APIName:"DanstanDToDoApp",
+            getTodosByUser:"/api/getTodoByUser/:username",
+            getTodosByID:"/api/getTodoById/:id",
+            createTodos:"/api/createTodo",
+            updateTodo:"/api/updateTodo",
+            deleteTodo:"/api/deleteTodo/:id"
+
         })
 
     })
+
 }
 
 module.exports=apiRoutes
