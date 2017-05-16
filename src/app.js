7640d689f48f70197c897d856fd930e9d0672820
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-05-16 10:22:39 +0300

    Adds the more api routes

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ import session from 'express-session'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import routers from './server/routes'
+import todosApi from './api/routes'
+import todoSeed from './api/controllers/setupController'
+import todosApiController from './api/controllers/todoApiController'
+import user_accountsApi from './api/controllers/accounts-usersController'
 
 console.time(">>>Application Start time: ");
 const MongoStore = connectMongo(session)
@@ -32,11 +36,13 @@ app.use(session({
         store: new MongoStore({ url: dbURL })
     })
 )
-
+user_accountsApi(app)
+todoSeed(app)
+todosApiController(app)
+todosApi(app)
 routers(app)
 
 app.listen(configs.testPort)
 
 console.log(">Server started running at http://localhost:"+configs.testPort)
-console.log(">Connected to Database at "+dbURL)
 console.timeEnd(">>>Application Start time: ");
