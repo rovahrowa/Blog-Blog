43fb36a52f0a556d38e1544284294ffcae709376
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-05-16 10:21:34 +0300

    Adds the Todo api endpints for create update todo

--- src/api/controllers/todoApiController.js
+++ src/api/controllers/todoApiController.js
@@ -1,112 +1,96 @@
 /**
  * Created by danstan on 5/7/17.
  */
-var Todos = require ('../models/todoModel');
-var bodyParser = require('body-parser');
+import collections from '../../server/common'
+import mongo from 'mongodb'
 
-module.exports =  function (app) {
+var todosApiController =  function (app) {
 
-
-    //pasrse json before handling the request
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
-    //API Locations
-
-    app.get('/', function (req,res) {
-
-        res.render('../views/login.ejs')
-
-    })
-
-    app.get('/api', function (req, res) {
-        res.json({
-
-            APIName:"DanstanDToDoApp",
-            getTodosByUser:"/api/getbyUser/:username",
-            getTodosByID:"/api/getbyID/:id",
-            createTodos:"/api/createTodo/",
-            updateTodo:"/api/updateTodo",
-            deleteTodo:"/api/deleteTodo/:id"
-
-        })
-
-    })
-
-    //Get todos for a user
-    app.get('/api/getbyUser/:username', function (req, res) {
-
-        Todos.find({username: req.params.username}, function (err, todos) {
-
-            if(err) throw err;
-            res.send(todos);
-        });
-    });
-    //get a particularTodo
-    app.get('/api/getbyID/:id', function (req, res) {
-
-        Todos.findById({_id: req.params.id}, function (err, todo) {
-
-            if (err) throw err;
-            res.send(todo)
-
-
-        })
-    })
-
-    app.put('/api/updateTodo', function (req, res) {
-
-            Todos.findByIdAndUpdate(req.body.id, {
-                todo:req.body.todo,
-                isDone:req.body.isDone,
-                hasAttachment:req.body.hasAttachment
-            }, function (err,todo) {
-
-                if (err) throw err;
-                res.send('Updated');
-            })
-
+    console.log("#####TodApi Collections Import :::::"+collections.confirmExport)
+    app.get('/api/getTodoByUser/:username', function (req,res) {
+        collections.todos.find().toArray((e,o)=>{
+            console.log("#-----------###################-------------#")
+            console.log("#-----------GET TODOS BY USER---------------#")
+            console.log("#-----------###################-------------#")
+            console.log(req.params.username+"'s "+"Todos:::")
+            console.log(o)
+            console.log("#-----------###################-------------#")
+            res.send(o)
         });
 
-
-
-    app.post('/api/createTodo', function (req, res) {
-
-        if (req.params.id) {
-
-            Todos.findByIdAndUpdate(req.body.id, {
-                todo:req.body.todo,
-                isDone:req.body.isDone,
-                hasAttachment:req.body.hasAttachment
-            }, function (err,todo) {
-
-                if (err) throw err;
-                res.send('Updated');
-            })
-
-        }
-        else {
-
-            var newTodo = Todos({
-                username:req.body.username,
-                todo:req.body.todo,
-                isDone:req.body.isDone,
-                hasAttachment:req.body.hasAttachment
-            })
-
-            newTodo.save(function (err) {
-                if(err) throw err;
-                res.send('Created')
-            });
-        }
     })
 
-    app.delete('/api/deleteTodo/:id', function (req, res) {
-
-        Todos.findByIdAndRemove(req.params.id, function (err) {
-            if (err) throw err;
-            res.send('deleted')
-
+    app.get('/api/getTodoById/:id', function (req,res) {
+        let o_id = new mongo.ObjectID(req.params.id);
+        collections.todos.find({'_id':o_id}).toArray(function (e, o) {
+            console.log("#-----------###################-------------#")
+            console.log("#-----------GET TODOS BY ID-----------------#")
+            console.log("#-----------###################-------------#")
+            console.log("Todo with ID: "+req.params.id)
+            console.log(o)
+            console.log("#-----------###################-------------#")
+            res.send(o)
         })
+
     })
+
+   app.post('/api/createTodo', (req,res)=>{
+       let newTodo={
+           username:req.body.username,
+           todo:req.body.todo,
+           isDone:req.body.isDone,
+           hasAttachment:req.body.hasAttachment
+       }
+
+       collections.todos.insert(newTodo, {safe: true}, function(e,o){
+           if (!e){
+               console.log("#-----------###################-------------#")
+               console.log("#-----------New Todo Added-------------#")
+               console.log("#-----------###################-------------#")
+               console.log("Todo Created as:::")
+               console.log("#-----------###################-------------#")
+               console.log(o)
+               res.send("Created")
+
+           }
+           else throw errors
+       })
+   })
+    app.put('/api/updateTodo', (req,res)=>{
+        let o_id = new mongo.ObjectID(req.body.id);
+        collections.todos.find({_id:o_id}).toArray(function(e, o){
+            if (o[0]==null){
+                res.send("Object no found")
+            }
+
+            else {
+                console.log(o[0])
+                o[0].username= req.body.username;
+                o[0].isDone=req.body.isDone;
+                o[0].todo=req.body.todo;
+                o[0].hasAttachment= req.body.hasAttachment;
+                console.log(o[0])
+                collections.todos.save(o[0], {safe: true}, function (e) {
+                    if (e) throw(e);
+                    else {
+                        console.log("#-----------###################-------------#")
+                        console.log("#-----------Todo Updated--------------------#")
+                        console.log("#-----------###################-------------#")
+                        console.log("Todo Updated as:::")
+                        console.log("#-----------###################-------------#")
+                        console.log(o[0])
+                        res.send("Updated")
+                    }
+
+                });
+            }
+        });
+
+
+    })
+
+
 
 }
+
+module.exports=todosApiController
